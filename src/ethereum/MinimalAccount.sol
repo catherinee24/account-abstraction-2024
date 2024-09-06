071df7af9529f084032efb154fbe41d39c7d8395
catherinee24 <catherinemendez24@gmail.com>
2024-09-06 18:35:52 -0500

    EIP 191: Signing messages

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -3,13 +3,30 @@ pragma solidity 0.8.24;
 
 import { IAccount } from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import { PackedUserOperation } from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MinimalAccount is IAccount {
+contract MinimalAccount is IAccount, Ownable {
+    constructor() Ownable(msg.sender) { }
+
+    //A signature is valid, if it is the MinimalAccount contract owner
     function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
     )
         external
-        returns (uint256 validationData);
+        returns (uint256 validationData)
+    {
+        _validateSignature(userOp, userOpHash);
+    }
+
+    // this userOpHash its going to be the EIP 191 version of the signed hash.
+    function _validateSignature(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    )
+        internal
+        view
+        returns (uint256 validationData)
+    { }
 }
